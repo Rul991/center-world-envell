5d946d8b1571a83d76b01c5728627ac9d57f2a22
Rul991 <archerartem6@gmail.com>
2025-07-18 11:54:40 +0300

    add secret

--- src/components/other/Header/Header.tsx
+++ src/components/other/Header/Header.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react'
 import styles from './Header.module.scss'
+import { MORGART_CHANCE } from '../../../utils/consts'
 
 const Header = () => {
+    const [chance] = useState(Math.random())
+    const [logo, setLogo] = useState('images/logo.png')
+
+    useEffect(() => {
+        const isMorgart = chance < MORGART_CHANCE
+
+        if(isMorgart) {
+            const href = `images/morgart.png`
+            const logoLink = document.querySelector('#logo') as HTMLLinkElement
+
+            if(logoLink) {
+                logoLink.href = href
+            }
+
+            document.title = 'Менять правила все таки можно!'
+            setLogo(href)
+        }
+    }, [])
+
     return <header className={styles.header}>
-        <img src="images/logo.png" alt="" />
+        <img src={logo} alt="logo" />
         <span>Центр мира</span>
     </header>
 }
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -4,3 +4,4 @@ export const MILLSECONDS_IN_SECOND = 1000
 export const MAIN_PAGE_ID = 0
 export const CHAT_PAGE_ID = 1
 export const SETTINGS_PAGE_ID = 2
+export const MORGART_CHANCE = 0.01
